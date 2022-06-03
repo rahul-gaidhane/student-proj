1ca3fa5a03683ac1a244893b1f09c79b712a80ec
rahul-gaidhane <rahulgaidhane98@gmail.com>
2022-06-03 18:02:43 +0530

    disable the swagger config in prod

--- src/main/java/in/example/config/SwaggerConfig.java
+++ src/main/java/in/example/config/SwaggerConfig.java
@@ -1,9 +1,11 @@
 package in.example.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Profile("{!prod}")	//this disables the swagger in prod profile
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
