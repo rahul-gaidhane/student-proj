abb382a9e72475e9f78ecbf7707bf24401eb1e91
rahul-gaidhane <rahulgaidhane98@gmail.com>
2022-06-14 12:52:10 +0530

    Add method name and class name in logging message

--- src/main/java/in/example/config/LoggingAopConfig.java
+++ src/main/java/in/example/config/LoggingAopConfig.java
@@ -1,5 +1,6 @@
 package in.example.config;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -14,9 +15,9 @@ public class LoggingAopConfig {
 	@Before("within(@org.springframework.stereotype.Repository *)" +
 	        " || within(@org.springframework.stereotype.Service *)" +
 	        " || within(@org.springframework.web.bind.annotation.RestController *)")
-    public void springBeanPointcut() {
+    public void springBeanPointcut(JoinPoint joinPoint) {
 		
-		log.debug("In logging config before any method of class annotated with Repository, Service or RestController...");
+		log.debug("Advice executed for the before the method[{}] of the class[{}] gets executed...", joinPoint.getSignature().getName(), joinPoint.getTarget().getClass().getCanonicalName());
     }
 	
 	
